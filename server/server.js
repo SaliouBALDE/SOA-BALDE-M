e650a775326d244d63d4b03bddc3ce2f509e07aa
SaliouBALDE <mamadou.balde9589@gamil.com>
2025-01-13 15:12:09 +0100

    Idel commit

--- server/server.js
+++ server/server.js
@@ -11,10 +11,25 @@ const server = http.createServer(app);
 // Connect to mongoDB
 connectDB();
 
+//Connection to mongodb Atlas
 mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atlas');
     server.listen(PORT, () => {
         console.log(`Node.js App running on port ${PORT} ...`)
     });
 })
+/*
+//Connection to mongodb Compass
+mongoose.connect('mongodb://0.0.0.0:27017/test');
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB Compass');
+    server.listen(PORT, () => {
+        console.log(`Node.js App running on port ${PORT} ...`)
+    });
+})
+
+conn.on('error', () => {
+    console.log('Connected to MongoDB Compass failled!');
+    process.exit();
+})*/
 
